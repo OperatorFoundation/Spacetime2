1c8a3c981122a5b670f64bb87972d6eca3378b3d
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-01-16 15:10:30 -0600

    Made References Codable (with a cavaet)

--- Sources/Universe/Interactions/Persistence/Reference.swift
+++ Sources/Universe/Interactions/Persistence/Reference.swift
@@ -9,13 +9,19 @@ import Foundation
 
 public class Reference<T> where T: Codable
 {
+    enum CodingKeys: String, CodingKey
+    {
+        case identifier
+        case object
+    }
+
     static public func load(universe: Universe, identifier: UInt64) throws -> Reference<T>
     {
         let object: T = try universe.load(identifier: identifier)
         return Reference(universe: universe, identifier: identifier, object: object)
     }
 
-    public let universe: Universe
+    public let universe: Universe?
     public let identifier: UInt64
     public let object: T
     public let type: String
@@ -33,10 +39,10 @@ public class Reference<T> where T: Codable
 
         try self.save()
 
-        try self.universe.append(type: type, identifier: identifier)
+        try universe.append(type: type, identifier: identifier)
     }
 
-    public init(universe: Universe, identifier: UInt64, object: T)
+    public init(universe: Universe?, identifier: UInt64, object: T)
     {
         self.universe = universe
         self.identifier = identifier
@@ -44,19 +50,57 @@ public class Reference<T> where T: Codable
         self.type = "\(Swift.type(of: object))"
     }
 
+    public required init(from decoder: Decoder) throws
+    {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.identifier = try container.decode(UInt64.self, forKey: CodingKeys.identifier)
+        let object = try container.decode(T.self, forKey: CodingKeys.object)
+        self.object = object
+
+        self.type = "\(Swift.type(of: object))"
+        self.universe = nil
+    }
+
+    public func encode(to encoder: Encoder) throws
+    {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.identifier, forKey: CodingKeys.identifier)
+        try container.encode(self.object, forKey: CodingKeys.object)
+    }
+
     public func save() throws
     {
-        try self.universe.save(identifier: self.identifier, object: self.object)
+        guard let universe = self.universe else
+        {
+            throw ReferenceError.noUniverse
+        }
+
+        try universe.save(identifier: self.identifier, object: self.object)
     }
 
     public func delete() throws
     {
-        let _ = try self.universe.delete(identifier: self.identifier)
-        try self.universe.delete(type: self.type, identifier: self.identifier)
+        guard let universe = self.universe else
+        {
+            throw ReferenceError.noUniverse
+        }
+
+        let _ = try universe.delete(identifier: self.identifier)
+        try universe.delete(type: self.type, identifier: self.identifier)
     }
 
     public func copy() throws -> Reference<T>
     {
-        return try Reference(universe: self.universe, object: self.object)
+        guard let universe = self.universe else
+        {
+            throw ReferenceError.noUniverse
+        }
+
+        return try Reference(universe: universe, object: self.object)
     }
+}
+
+public enum ReferenceError: Error
+{
+    case noUniverse
 }
